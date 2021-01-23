a8c9110449aab245b416fa252beaffc1e71196cb
Yohuan <b00502053@ntu.edu.tw>
2021-01-23 23:26:47 +0800

    implement todo in-memory storage

--- server/src/storage/todo/inMemoryStorage.js
+++ server/src/storage/todo/inMemoryStorage.js
@@ -1,5 +1,5 @@
 const _createInMemoryStorage = (initialTodos = []) => {
-  const _todos = initialTodos;
+  let _todos = initialTodos;
 
   const getAllTodos = async () => {
     return _todos;
@@ -9,9 +9,36 @@ const _createInMemoryStorage = (initialTodos = []) => {
     _todos.push(todo);
   };
 
+  const getTodoById = async (todoId) => {
+    const targetTodo = _todos.find(todo => todo.id === todoId);
+    return !targetTodo ? null : targetTodo;
+  };
+
+  const deleteTodoById = async (todoId) => {
+    _todos = _todos.filter(todo => todo.id !== todoId);
+  };
+
+  const updateTodoById = async (todoId, { description = null, isCompleted = null } = {}) => {
+    const targetIdx = _todos.findIndex(todo => todo.id === todoId);
+    if (targetIdx < 0) {
+      // TODO: throw customized error
+      throw new Error('todo not found');
+    }
+
+    if (description) {
+      _todos[targetIdx].description = description;
+    }
+    if (isCompleted) {
+      _todos[targetIdx].isCompleted = isCompleted;
+    }
+  };
+
   return {
+    deleteTodoById,
     getAllTodos,
+    getTodoById,
     saveTodo,
+    updateTodoById,
   }
 };
 
