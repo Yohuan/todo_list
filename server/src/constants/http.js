c7559c37568fcf5072ae049610013f1980b7708a
Yohuan <b00502053@ntu.edu.tw>
2021-01-24 11:24:27 +0800

    make status code constant

--- server/src/constants/http.js
+++ server/src/constants/http.js
@@ -2,6 +2,16 @@ const HttpHeader = {
   LOCATION: 'Location',
 };
 
+const StatusCode = {
+  OK_200: 200,
+  CREATED_201: 201,
+  NO_CONTENT_204: 204,
+  BAD_REQUEST_400: 400,
+  NOT_FOUND_404: 404,
+  INTERNAL_SERVER_ERROR_500: 500,
+};
+
 module.exports = {
   HttpHeader,
+  StatusCode,
 };
--- server/src/controllers/todo/handleCreateTodo.js
+++ server/src/controllers/todo/handleCreateTodo.js
@@ -1,4 +1,5 @@
 const TodoService = require('@server/services/todo');
+const { StatusCode } = require('@server/constants/http');
 const { TodoErrorCode } = require('@server/constants/error');
 const { HttpHeader } = require('@server/constants/http');
 const { generateTodoUrl } = require('@server/utils/todo');
@@ -10,14 +11,14 @@ const handleCreateTodo = async (req, res) => {
   try {
     todoId = await TodoService.createTodo(todoDescription);
   } catch (err) {
-    res.status(500).json({
+    res.status(StatusCode.INTERNAL_SERVER_ERROR_500).json({
       code: TodoErrorCode.CREATION_ERROR,
       message: 'Cannot create a todo.',
     });
   }
 
   res.set(HttpHeader.LOCATION, generateTodoUrl(todoId));
-  res.status(201).json({ todoId });
+  res.status(StatusCode.CREATED_201).json({ todoId });
 };
 
 module.exports = handleCreateTodo;
