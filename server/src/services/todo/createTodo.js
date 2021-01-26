f468ecc2bd721dd61524a4c8ffbc1ad75b16b777
Yohuan <yohuanyang@synology.com>
2021-01-26 19:49:14 +0800

    get storage from registry

--- server/src/services/todo/createTodo.js
+++ server/src/services/todo/createTodo.js
@@ -1,8 +1,9 @@
 const { createTodo } = require('@server/utils/todo');
-const { todoInMemoryStorage } = require('@server/storage/todo');
+const { todoStorageRegistry } = require('@server/storage/todo');
 
 module.exports = async todoDescription => {
   const todo = createTodo(todoDescription);
-  await todoInMemoryStorage.saveTodo(todo);
+  const todoStorage = todoStorageRegistry.getStorage();
+  await todoStorage.saveTodo(todo);
   return todo.id;
 };
