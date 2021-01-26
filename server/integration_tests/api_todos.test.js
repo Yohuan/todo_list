5ddb687e72a66c0d2a9a7ffe558943c11ac3bd87
Yohuan <b00502053@ntu.edu.tw>
2021-01-26 23:02:38 +0800

    test GET & POST /api/todos

---
+++ server/integration_tests/api_todos.test.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+
+const request = require('supertest');
+const YAML = require('yamljs');
+
+const { buildApp } = require('@server/utils/app');
+const { createTodoInMemoryStorage, todoStorageRegistry } = require('@server/storage/todo');
+const { resetForTesting } = require('@server/storage/todo/storageRegistry');
+
+const _OPENAPI_SPEC_FILE = path.join(__dirname, '../config/openapi.yaml');
+
+const _INITIAL_TODOS = [
+  {
+    id: 'abcdef',
+    description: 'first todo',
+    isCompleted: false,
+  },
+  {
+    id: 'ghijkl',
+    description: 'second todo',
+    isCompleted: true,
+  },
+];
+
+const _prepareTestingApp = async () => {
+  const apiSpecStr = fs.readFileSync(_OPENAPI_SPEC_FILE, 'utf8');
+  const apiSpec = YAML.parse(apiSpecStr);
+  return buildApp({ apiSpec });
+};
+
+const _initializeTodoStorage = () => {
+  resetForTesting();
+  const todoInMemoryStorage = createTodoInMemoryStorage(_INITIAL_TODOS);
+  todoStorageRegistry.register(todoInMemoryStorage);
+};
+
+let app;
+beforeAll(async () => {
+  app = await _prepareTestingApp();
+});
+
+beforeEach(() => {
+  _initializeTodoStorage();
+});
+
+describe('GET /api/todos', () => {
+  it('should return 200 with all todos', () => {
+    return request(app)
+      .get('/api/todos')
+      .expect('Content-Type', /application\/json/)
+      .expect(200)
+      .expect(res => {
+        expect(res.body).toEqual(_INITIAL_TODOS);
+      });
+  });
+});
+
+describe('POST /api/todos', () => {
+  it('should return 201 with the created todo ID', () => {
+    return request(app)
+      .post('/api/todos')
+      .send({ todoDescription: 'a new todo' })
+      .expect('Content-Type', /application\/json/)
+      .expect(201)
+      .expect(res => {
+        const { todoId } = res.body;
+        expect(todoId).toBeString();
+        expect(todoId.length).toBe(6);
+      });
+  });
+});
