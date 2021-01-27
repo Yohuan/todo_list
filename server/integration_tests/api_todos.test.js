f8847a2af2b0ae2bea921537a746ccda6d1161d7
Yohuan <yohuanyang@synology.com>
2021-01-27 18:42:57 +0800

    test internal error

--- server/integration_tests/api_todos.test.js
+++ server/integration_tests/api_todos.test.js
@@ -7,6 +7,7 @@ const YAML = require('yamljs');
 const { buildApp } = require('@server/utils/app');
 const { createTodoInMemoryStorage, todoStorageRegistry } = require('@server/storage/todo');
 const { resetForTesting } = require('@server/storage/todo/storageRegistry');
+const { TodoErrorCode } = require('@server/constants/error');
 
 const _OPENAPI_SPEC_FILE = path.join(__dirname, '../config/openapi.yaml');
 
@@ -35,17 +36,21 @@ const _initializeTodoStorage = () => {
   todoStorageRegistry.register(todoInMemoryStorage);
 };
 
+const _initializeFailedTodoStorage = () => {
+  resetForTesting();
+  const failedTodoStorage = null; // any operation on this storage will fail
+  todoStorageRegistry.register(failedTodoStorage);
+};
+
 let app;
 beforeAll(async () => {
   app = await _prepareTestingApp();
 });
 
-beforeEach(() => {
-  _initializeTodoStorage();
-});
-
 describe('GET /api/todos', () => {
   it('should return 200 with all todos', () => {
+    _initializeTodoStorage();
+
     return request(app)
       .get('/api/todos')
       .expect('Content-Type', /application\/json/)
@@ -54,10 +59,24 @@ describe('GET /api/todos', () => {
         expect(res.body).toEqual(_INITIAL_TODOS);
       });
   });
+  it('should return 500 with error code when storage fails', () => {
+    _initializeFailedTodoStorage();
+
+    return request(app)
+      .get('/api/todos')
+      .expect('Content-Type', /application\/json/)
+      .expect(500)
+      .expect(res => {
+        const { code } = res.body;
+        expect(code).toBe(TodoErrorCode.FETCHING_ERROR);
+      });
+  });
 });
 
 describe('POST /api/todos', () => {
   it('should return 201 with the created todo ID', () => {
+    _initializeTodoStorage();
+
     return request(app)
       .post('/api/todos')
       .send({ todoDescription: 'a new todo' })
@@ -67,6 +86,19 @@ describe('POST /api/todos', () => {
         const { todoId } = res.body;
         expect(todoId).toBeString();
         expect(todoId.length).toBe(6);
+      });
+  });
+  it('should return 500 with error code when storage fails', () => {
+    _initializeFailedTodoStorage();
+
+    return request(app)
+      .post('/api/todos')
+      .send({ todoDescription: 'a new todo' })
+      .expect('Content-Type', /application\/json/)
+      .expect(500)
+      .expect(res => {
+        const { code } = res.body;
+        expect(code).toBe(TodoErrorCode.CREATION_ERROR);
       });
   });
 });
