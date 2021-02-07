55c2e93d6c427911150adc5327bb65e6c901326d
Yohuan <b00502053@ntu.edu.tw>
2021-02-07 20:32:37 +0800

    parse test coverage

---
+++ scripts/extract_test_coverage_from_report.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+
+const { ArgumentParser } = require('argparse');
+
+const parser = new ArgumentParser({
+  description: 'Parse jest test coverage',
+});
+
+parser.add_argument('-f', '--coverage-file', {
+  help: 'The jest test coverage file in json format',
+  dest: 'inputFile',
+  type: 'str',
+  required: true,
+});
+
+parser.add_argument('-t', '--coverage-type', {
+  help: 'The target coverage type',
+  dest: 'coverageType',
+  type: 'str',
+  required: true,
+});
+
+const args = parser.parse_args();
+const { inputFile, coverageType } = args;
+
+const rawData = fs.readFileSync(inputFile);
+const { total: totalCoverage } = JSON.parse(rawData);
+
+console.log(totalCoverage[coverageType].pct);
